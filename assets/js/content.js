7da963bdbf9545c0bb472a223ebd078e8b796f9b
MakenaMwenda <agatha.mutheu@gmail.com>
2025-06-09 12:19:43 +0530

    Add files via upload

---
+++ assets/js/content.js
@@ -0,0 +1,79 @@
+export const portfolioItems = [
+  {
+    title: "SQL 123",
+    year: "2025",
+    link: "sql.html",
+    image: {
+      src: "assets/img/portfolio/sql-portfolio-img.jpeg",
+      alt: "",
+    },
+    category: "sql",
+  },
+  {
+    title: "The Varsity Newspaper",
+    year: "2025",
+    link: "varsity-newspaper.html",
+    image: {
+      src: "assets/img/portfolio/tableau-portfolio-img.jpeg",
+      alt: "",
+    },
+    category: "tableau",
+  },
+  {
+    title: "Low Tides Properties",
+    year: "2025",
+    link: "low-tides.html",
+    image: {
+      src: "assets/img/portfolio/python-portfolio-img.jpeg",
+      alt: "",
+    },
+    category: "python",
+  },
+  {
+    title: "Practice Exploratory",
+    year: "2025",
+    link: "practice-exploratory.html",
+    image: {
+      src: "assets/img/portfolio/powerbi-portfolio-img.jpeg",
+      alt: "",
+    },
+    category: "powerbi",
+  },
+  {
+    title: "Excel Work",
+    year: "2025",
+    link: "excel-work.html",
+    image: {
+      src: "assets/img/portfolio/excel-portfolio-img.jpeg",
+      alt: "",
+    },
+    category: "excel",
+  },
+];
+
+export const iconBoxes = [
+  {
+    iconClass: "bi bi-activity",
+    title: "51+",
+    description: "Personal Data Analysis Project",
+    link: "#",
+  },
+  {
+    iconClass: "bi bi-gem",
+    title: "1+ Year",
+    description: "Professional + Freelancing + Internship experience",
+    link: "#",
+  },
+  {
+    iconClass: "bi bi-easel",
+    title: "2+ Years",
+    description: "Experience as a Private Tutor",
+    link: "#",
+  },
+  {
+    iconClass: "bi bi-command",
+    title: "3+ Years",
+    description: "Research Experience (2 Published Research Papers)",
+    link: "#",
+  },
+];
